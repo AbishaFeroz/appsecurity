b19ab5539bf83f739e934da8fc0285737c1fe4b0
Feroz Khan <SnferozKhan.S@flex.com>
2021-10-15 12:29:04 +0530

    authentication changes

--- src/main/java/com/microservice/security/appsecurity/AppsecurityApplication.java
+++ src/main/java/com/microservice/security/appsecurity/AppsecurityApplication.java
@@ -1,9 +1,23 @@
 package com.microservice.security.appsecurity;
 
+import com.microservice.security.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
+//@SpringBootApplication(exclude = {
+//		DataSourceAutoConfiguration.class,
+//		DataSourceTransactionManagerAutoConfiguration.class,
+//		HibernateJpaAutoConfiguration.class
+//})
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@ComponentScan({"com.microservice.security", "com.microservice.security.configuration", "com.microservice.security.resource", "com.microservice.security.appsecurity"})
 public class AppsecurityApplication {
 
 	public static void main(String[] args) {
